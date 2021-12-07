7c09f6729492ad69f8c70a225667dcf233920071
ormeno <rodrigo.ormeno.jabonero@gmail.com>
2021-12-07 14:07:36 +0100

    Segundo commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const hashPayment = require('./lib/hashPayment')
+
+app.set('port',3000)
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.engine('.hbs',exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}))
+app.set('view engine', '.hbs')
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.post('/', hashPayment, (req, res) => {
+    alert('sig0')
+    const { recipient, amount, nonce, defaultAccount, hash, contractAddress} = req.body
+    const contentRes = {
+        recipient,
+        amount,
+        nonce,
+        defaultAccount,
+        hash,
+        contractAddress
+    }
+    alert('s1')
+    console.log(contentRes)
+    res.render('index2', {contentRes})
+})
+
+
+app.listen(app.get('port'), () => {
+    console.log('Server is running')
+})
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    contractAddress: '0xE54CD00fb51CE6C3cB16D12f543E8e35e0faD491'
+}
